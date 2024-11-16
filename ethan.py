3af2c4d6def5ad7728b6b93148df8849033e5a31
Irish <ethan@irish.repair>
2024-11-16 14:50:13 -0800

    added photo for test

--- ethan.py
+++ ethan.py
@@ -1,1 +1,15 @@
 #ill put my code here!
+
+# cantaliver beam
+%matplotlib ipympl
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()
+img = plt.imread('cantaliver_beam.png')
+
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(3*x)
+ax.plot(x, y)
+
+plt.show()
