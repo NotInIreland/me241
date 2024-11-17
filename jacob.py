3b3ba21d0449c36f6e2bb0f8c8f16b7e924b6b28
Jacobmisuda <jacobmisuda@gmail.com>
2024-11-16 18:03:44 -0800

    Rudimentarty cantilevel beam values

--- jacob.py
+++ jacob.py
@@ -2,12 +2,26 @@
 #test
 import numpy as np
 import scipy as sp
+from sympy import symbols, integrate
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+x, p, c1, c2 = symbols('x, p, c1, c2')
 def slopecantilever(x, p):
     m = -p * x
-    theta = sp.integrate(m)
+    theta = integrate(m, x) + c1
+    return theta
 
-x = 5
-p = 50
-slopecantilever(x, p)
+theta = slopecantilever(x, p)
+
+
+def deflcantilever(x, p):
+    deflec = integrate(theta, x) + c2
+    return deflec
+
+deflec = deflcantilever(x, p)
+
+thetaval = theta.subs({x: 5, p: 50, c1: 0})
+print(thetaval)
+
+deflecval = deflec.subs({x: 5, p: 50, c1:0, c2: 0})
+print(deflecval)
