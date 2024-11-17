3495d92a4efc1e5b7c995183024b9c6864b4fa50
Jacobmisuda <jacobmisuda@gmail.com>
2024-11-16 17:54:44 -0800

    Test of integral

--- jacob.py
+++ jacob.py
@@ -1,3 +1,13 @@
 #jacob put your code here!
 #test
+import numpy as np
+import scipy as sp
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+def slopecantilever(x, p):
+    m = -p * x
+    theta = sp.integrate(m)
 
+x = 5
+p = 50
+slopecantilever(x, p)
