f7e3d71320ca1ac2833b0c56340dd23d415b5bb5
Josie-Soles <josephine.soles@wsu.edu>
2024-12-04 12:58:37 -0800

    added file

--- josie.py
+++ josie.py
@@ -1,1 +1,9 @@
 #josie put your code here!
+import numpy as np
+def I(l1, w1, hh, hw, w2, l2):
+    term1=(w1*(l1**3))/12
+    term2=(hh*(hw**3))/12
+    term3=(w2*(l2**3))/12
+    I=term1+term2+term3
+    return I
+
