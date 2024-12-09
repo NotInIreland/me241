24ebec748631a48d203eaa3e1d353856fef06d25
Jacobmisuda <jacobmisuda@gmail.com>
2024-12-08 17:40:53 -0800

    working finally

--- jacob.py
+++ jacob.py
@@ -1,13 +1,13 @@
 #jacob put your code here!
 import numpy as np
 import scipy as sp
-from sympy import symbols, integrate
+from sympy import symbols, integrate, solve
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import os
 print(os.getcwd())
 
-x, p, c1, c2, c3, c4, m0 = symbols('x p c1 c2 c3 c4 m0')
+x, p, c1, c2, c3, c4, m0, w, l = symbols('x p c1 c2 c3 c4 m0 w l')
 
 data = np.loadtxt('https://raw.githubusercontent.com/NotInIreland/me241/refs/heads/main/W%20flange%20beams%20sae%20table.csv', dtype=str, skiprows=5, delimiter=',')
 id = data[:,0] #identification
@@ -16,16 +16,16 @@ width = data[:,2]
 webThick = data[:,3] #web thickness
 fThick = data[:,4] #flange thickness
 weight = data[:,6]
-dimension = input('What are the dimensons of this beam? The naming convention is W 27 x 178')
+dimension = input("What are your beam dimensions? An example input is W 27 x 178")
 
 beamtype = input("What type of beam are you solving for? Your options are cantilever(c)")
 beamload = input("What kind of load is on the beam? Your options are intermediate load(IL), uniformly distributed load(UL), and moment(M)")
-E = input("What is young's modulus for this beam (E value)? This is with assumed units of psi")
+E = float(input("What is young's modulus for this beam (E value)? This is with assumed units of psi"))
 beamtype = beamtype.lower()
 beamload = beamload.lower()
 
-def I(dimension):
-    for i in range(0, n):
+def inertia(dimension):
+    for i in range(0, len(id)):
         d1 = float(depth[i])
         w1 = float(width[i])
         wt1 = float(webThick[i])
@@ -38,9 +38,9 @@ def I(dimension):
             I = 'Unreadable input. Please input dimesion of standard designation including spaces. Example: W # x #.'
     return I
 
-print(I)
+I = inertia(dimension)
 
-def W(dimension):
+def Weight(dimension):
     for i in range(0, len(id)):
         weight1 = weight[i]
         if id[i] == dimension:
@@ -48,13 +48,13 @@ def W(dimension):
             break
         else:
             W = 'Unreadable input. Please input dimesion of standard designation including spaces. Example: W # x #.'
-    return W
+    return weight
 
 def slopecalculate(mgiven, E, I):
-    return integrate(mgiven / (E * I), x) + c1
+    return integrate(mgiven / (E * I), l) + c1
      
 def defleccalculate(slopeq):
-    return integrate(slopeq, x) + c2 
+    return integrate(slopeq, l) + c2 
 
 def values(mgiven, E, I):
     slopeq = slopecalculate(mgiven, E, I)
@@ -78,8 +78,8 @@ if beamtype == 'c':
         p1 = float(input("What is the load on the beam in units of lbs"))
         mgiven = -p1 * (l1 - x)
         slope, deflection = values(mgiven, E, I)
-        print(slope)
-        print(deflection)
+        print(f'The slope equation is {slope}')
+        print(f'The deflection equation is {deflection}')
     elif beamload == 'ul':
         l2 = float(input("What is the length of the beam (in ft)"))
         x2 = float(input("Where do you want to measure the deflection of the beam (in ft)"))
@@ -91,3 +91,4 @@ if beamtype == 'c':
         x3 = float(input("Where is the moment on the beam, starting with 0 on the left side (in ft)"))
         m03 = float(input("What is the applied moment on this beam, in units of ft * lbs"))
         mgiven = -m03
+
