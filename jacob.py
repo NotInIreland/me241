c48225dfcfd3cf71ef3f2324203fdf18e6bb7138
Jacobmisuda <jacobmisuda@gmail.com>
2024-12-11 19:13:06 -0800

    fixed values

--- jacob.py
+++ jacob.py
@@ -19,7 +19,7 @@ weight = data[:,6]
 dimension = input("What are your beam dimensions? An example input is W 27 x 178")
 
 beamtype = input("What type of beam are you solving for? Your options are cantilever(c)")
-beamload = input("What kind of load is on the beam? Your options are intermediate load(IL), uniformly distributed load(UL), and moment(M)")
+beamload = input("What kind of load is on the beam? Your options are intermediate load(IL), uniformly distributed load(UL), triangular distributed load(TL), and moment(M)")
 E = float(input("What is young's modulus for this beam (E value)? This is with assumed units of psi"))
 beamtype = beamtype.lower()
 beamload = beamload.lower()
@@ -42,21 +42,21 @@ I = inertia(dimension)
 
 def Weight(dimension):
     for i in range(0, len(id)):
-        weight1 = weight[i]
+        weight1 = float(weight[i])
         if id[i] == dimension:
-            W = float(weight1)
+            weight = float(weight1)
             break
         else:
-            W = 'Unreadable input. Please input dimesion of standard designation including spaces. Example: W # x #.'
+            weight = 'Unreadable input. Please input dimesion of standard designation including spaces. Example: W # x #.'
     return weight
 
 def slopecalculate(mgiven, E, I):
-    return integrate(mgiven / (E * I), l) + c1
+    return integrate(mgiven / (E * I), x) + c1
      
 def defleccalculate(slopeq):
-    return integrate(slopeq, l) + c2 
+    return integrate(slopeq, x) + c2 
 
-def values(mgiven, E, I):
+def values(mgiven, E, I, epointi):
     slopeq = slopecalculate(mgiven, E, I)
     deflecq = defleccalculate(slopeq)
 
@@ -66,29 +66,72 @@ def values(mgiven, E, I):
     c1val = solve(slopeb, c1)[0]
     c2val = solve(deflecb, c2)[0]
 
-    slope = slopeq.subs(c1, c1val)
-    deflection = deflecq.subs(c2, c2val)
+    slopefinal = slopeq.subs(c1, c1val)
+    deflectionfinal = deflecq.subs({c1: c1val, c2: c2val})
     
-    return slope, deflection
+    slopevalues = slopefinal.subs(x, epointi)
+    deflecvalues = deflectionfinal.subs(x, epointi)
+    return slopevalues.evalf(), deflecvalues.evalf()
 
 if beamtype == 'c':
     if beamload == 'il':
         l1 = float(input("What is the length of the beam (in ft)"))
+        l1i = l1 * 12
         x1 = float(input("Where is the load on the beam, starting with 0 on the left side (in ft)"))
+        x1i = x1 * 12
         p1 = float(input("What is the load on the beam in units of lbs"))
-        mgiven = -p1 * (l1 - x)
-        slope, deflection = values(mgiven, E, I)
+        mgiven = -p1 * (l1i - x1i)
+        evalpoint = float(input("Where would you like to evaluate the deflection at, starting with 0 on the left side (in ft)"))
+        epointi = evalpoint * 12
+        slope, deflection = values(mgiven, E, I, epointi)
         print(f'The slope equation is {slope}')
         print(f'The deflection equation is {deflection}')
     elif beamload == 'ul':
         l2 = float(input("What is the length of the beam (in ft)"))
+        l2i = l2 * 12
         x2 = float(input("Where do you want to measure the deflection of the beam (in ft)"))
+        x2i = x2 * 12
         w2 = float(input("What is the magnitude of the distributed load on the beam in units of lbs / ft"))
         w2 = w2 * l2
-        mgiven = (-w * (l - x)**2) / 2
+        mgiven = (-w2 * (l2i - x2i)**2) / 2
+        evalpoint = float(input("Where would you like to evaluate the deflection at, starting with 0 on the left side (in ft)"))
+        epointi = evalpoint * 12
+        slope, deflection = values(mgiven, E, I, epointi)
+        wfrac = Weight(dimension)
+        beamweight = l2 * wfrac
+        print(f'The weight of the beam is {beamweight}')
+        print(f'The slope equation is {slope}')
+        print(f'The deflection equation is {deflection}')    
     elif beamload == 'm':
         l3 = float(input("What is the length of the beam (in ft)"))
+        l3i = l3 * 12
         x3 = float(input("Where is the moment on the beam, starting with 0 on the left side (in ft)"))
+        x3i = x3 * 12
         m03 = float(input("What is the applied moment on this beam, in units of ft * lbs"))
-        mgiven = -m03
+        m03i = m03 * 12
+        mgiven = -m03i
+        evalpoint = float(input("Where would you like to evaluate the deflection at, starting with 0 on the left side (in ft)"))
+        epointi = evalpoint * 12
+        slope, deflection = values(mgiven, E, I, epointi)
+        wfrac = Weight(dimension)
+        beamweight = l3 * wfrac
+        print(f'The weight of the beam is {beamweight}')
+        print(f'The slope equation is {slope}')
+        print(f'The deflection equation is {deflection}')
+    elif beamload == 'tl':
+        l4 = float(input("What is the length of the beam (in ft)"))
+        l4i = l4 * 12
+        x4= float(input("Where do you want to measure the deflection of the beam (in ft)"))
+        x4i = x4 * 12
+        w4 = float(input("What is the magnitude of the distributed load on the beam in units of lbs / ft"))
+        w4 = w4 * l4
+        mgiven = (-1 / 6) * w4 * (x4i**3 / l4i)
+        evalpoint = float(input("Where would you like to evaluate the deflection at, starting with 0 on the left side (in ft)"))
+        epointi = evalpoint * 12
+        slope, deflection = values(mgiven, E, I, epointi)
+        wfrac = Weight(dimension)
+        beamweight = l4 * wfrac
+        print(f'The weight of the beam is {beamweight}')
+        print(f'The slope equation is {slope}')
+        print(f'The deflection equation is {deflection}')
 
